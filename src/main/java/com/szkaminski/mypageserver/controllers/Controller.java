4b3ef208fddd9bc082f515b753b7ed398d3a283d
Szymon Kamiński <kamin.szymon@gmail.com>
2019-07-17 11:36:48 +0200

    list of example comments

--- src/main/java/com/szkaminski/mypageserver/controllers/Controller.java
+++ src/main/java/com/szkaminski/mypageserver/controllers/Controller.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 
 @RestController
 public class Controller {
@@ -12,5 +13,14 @@ public class Controller {
     @GetMapping("/getComment")
     public Comment getComment(){
         return new Comment("Adam", LocalDateTime.of(2018,2,22,3,42), "Good job");
+    }
+
+    @GetMapping("/getListOfComments")
+    public ArrayList<Comment> getList(){
+        ArrayList<Comment> comments = new ArrayList<>();
+        comments.add(new Comment("Adam", LocalDateTime.of(2018,2,22,3,42), "Good job"));
+        comments.add(new Comment("Sławek", LocalDateTime.of(2018,3,11,13,22), "Best regards from work"));
+
+        return comments;
     }
 }
